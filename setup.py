fda831f555a5b9dac612b8744e3e923fb21c4529
maxgertzen <maxgertzen@gmail.com>
2021-08-12 02:41:59 +0300

    added setup.py file

--- setup.py
+++ setup.py
@@ -7,6 +7,21 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'flask',
+        'click==8.0.1',
+        'colorama==0.4.4',
+        'dnspython==1.16.0',
+        'Flask==2.0.1',
+        'Flask-Cors==3.0.10',
+        'Flask-HTTPAuth==4.4.0',
+        'itsdangerous==2.0.1',
+        'Jinja2==3.0.1',
+        'MarkupSafe==2.0.1',
+        'mongoengine==0.23.1',
+        'pymongo==3.12.0',
+        'python-dateutil==2.8.2',
+        'python-dotenv==0.19.0',
+        'six==1.16.0',
+        'typing-extensions==3.10.0.0',
+        'Werkzeug==2.0.1'
     ],
 )
