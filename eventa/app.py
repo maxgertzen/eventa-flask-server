a2934d2f59487c38cd4503e726b031a66c83bff3
maxgertzen <maxgertzen@gmail.com>
2021-08-12 03:05:09 +0300

    created create_app function for bundling

--- eventa/app.py
+++ eventa/app.py
@@ -12,33 +12,37 @@ load_dotenv()
 UPLOAD_FOLDER = os.environ.get('IMAGES_UPLOAD', '/static/images')
 ALLOWED_EXTENSIONS = os.environ.get('ALLOWED_EXTENSIONS', {'png', 'jpg', 'jpeg', 'gif'})
 
-app = Flask(__name__, static_url_path='/static')
-cors = CORS(app, supports_credentials=True, resources={"*":{"origins":"http://localhost:3000"}})
 
-auth = HTTPBasicAuth()
+def create_app():
+    app = Flask(__name__, static_url_path='/static')
+    cors = CORS(app, supports_credentials=True, resources={"*":{"origins":"http://localhost:3000"}})
 
-app.config["IMAGES_UPLOAD"] = UPLOAD_FOLDER
-app.config['CORS_HEADERS'] = 'content-Type'
-app.secret_key = os.environ.get('SECRET', os.urandom(16).hex())
+    auth = HTTPBasicAuth()
+
+    app.config["IMAGES_UPLOAD"] = UPLOAD_FOLDER
+    app.config['CORS_HEADERS'] = 'content-Type'
+    app.secret_key = os.environ.get('SECRET', os.urandom(16).hex())
+
+    @auth.verify_password
+    def verify_password(user_id, password):
+        if s_auth.loads(session.get('X-Authenticated')) == user_id and \
+                check_password_hash(os.environ['PASS_HASH'], password):
+            return True
+        session['X-Authenticated'] = None
+        session.modified = True
+        return None
+
+    @auth.error_handler
+    def unauthorized():
+        return jsonify(error='unauthorized access'), 403
+
+    app.register_blueprint(auth_route, url_prefix="/auth")
+    app.register_blueprint(events_route, url_prefix="/events")
+
+    return app
 
 
-@auth.verify_password
-def verify_password(user_id, password):
-    if s_auth.loads(session.get('X-Authenticated')) == user_id and \
-            check_password_hash(os.environ['PASS_HASH'], password):
-        return True
-    session['X-Authenticated'] = None
-    session.modified = True
-    return None
-
-
-@auth.error_handler
-def unauthorized():
-    return jsonify(error='unauthorized access'), 403
-
-
-app.register_blueprint(auth_route, url_prefix="/auth")
-app.register_blueprint(events_route, url_prefix="/events")
+app = create_app()
 
 if __name__ == "__main__":
     app.run(port=5050, debug=True)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='eventa',
     version='1.0.0',
-    packages=find_packages(),
+    packages=find_packages(where='eventa'),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
